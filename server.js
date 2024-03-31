694ea4ee789fb851a52e3bbea3522c3100ab3020
fathimaththasneem <fathimaththasnee@gmail.com>
2024-03-31 07:58:25 +0530

    add initail code

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+// const config = require('./config/dbConfig'); 
+const app = express();
+// establish  connection with DB
+// config.connect();
+
+
+app.get('/health', (req,res) => {
+    res.json({ message: 'server up and running' });
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening to PORT:${process.env.PORT}`)
+})
